2b7abe60d22ece834238cff9f6a6e0555fd24b44
Akshat Dewan <akshatdewan@gmail.com>
2019-10-22 16:26:08 +0200

    run make with 32 threads

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -3,12 +3,12 @@
 # Prepare Kaldi
 cd kaldi/tools
 make clean
-make
+make -j 32
 ./extras/install_openblas.sh
 cd ../src
 make clean
 # make clean (sometimes helpful after upgrading upstream?)
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
-make depend
-make
+make -j 32 depend
+make -j 32
 cd ../../

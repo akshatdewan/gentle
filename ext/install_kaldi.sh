436aa669d38031ddec5afb11fc7343f546ee1f34
Akshat Dewan <akshatdewan@gmail.com>
2019-10-24 15:52:38 +0200

    not building all kaldi binaries

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -10,5 +10,5 @@ cd ../src
 # make clean (sometimes helpful after upgrading upstream?)
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
 make -j 32 depend
-make -j 32
+#make -j 32
 cd ../../

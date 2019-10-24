a79a1bf06d831dcfb4748739848e160ef6f476e5
Akshat Dewan <akshatdewan@gmail.com>
2019-10-24 10:07:18 +0200

    make clean disabled

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -6,7 +6,7 @@ make clean
 make -j 32
 ./extras/install_openblas.sh
 cd ../src
-make clean
+#make clean
 # make clean (sometimes helpful after upgrading upstream?)
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no --openblas-root=../tools/OpenBLAS/install
 make -j 32 depend

6324960ab67e0064186641b2c501e787ce2563e5
Akshat Dewan <akshatdewan@gmail.com>
2020-03-05 13:39:10 +0100

    timeout added for txt audio pairs that dont align well

--- align_scp.py
+++ align_scp.py
@@ -6,6 +6,29 @@ import sys
 
 import gentle
 
+import signal
+from contextlib import contextmanager
+from concurrent.futures import TimeoutError
+
+@contextmanager
+def timeout(time):
+    # Register a function to raise a TimeoutError on the signal.
+    signal.signal(signal.SIGALRM, raise_timeout)
+    # Schedule the signal to be sent after ``time``.
+    signal.alarm(time)
+
+    try:
+        yield
+    except TimeoutError:
+        pass
+    finally:
+        # Unregister the signal so it won't be triggered
+        # if the timeout is not reached.
+        signal.signal(signal.SIGALRM, signal.SIG_IGN)
+
+def raise_timeout(signum, frame):
+    raise TimeoutError
+
 parser = argparse.ArgumentParser(
         description='Align a transcript to audio by generating a new language model.  Outputs JSON')
 parser.add_argument(
@@ -25,6 +48,9 @@ parser.set_defaults(disfluency=False)
 parser.add_argument(
         '--log', default="INFO",
         help='the log level (DEBUG, INFO, WARNING, ERROR, or CRITICAL)')
+parser.add_argument(
+        '--lang', default="en",
+        help='language of alignment (en, fr)')
 #parser.add_argument(
 #        'audiofile', type=str,
 #        help='audio file')
@@ -46,31 +72,46 @@ def on_progress(p):
         logging.debug("%s: %s" % (k, v))
 
 
+lang = args.lang
+
 with open(args.scpfile,'r') as f:
     scp = f.readlines()
 
-resources = gentle.Resources()
+resources = gentle.Resources(lang)
 #logging.info("converting audio to 8K sampled wav")
+if lang == 'en':
+    logging.info("converting audio to 8K sampled wav")
+if lang == 'fr':
+    logging.info("converting audio to 16K sampled wav")
 
-for item in scp:
+def align_pair(item):
     try:
         txtfile,audiofile = item.split()
         with open(txtfile) as fh:
             transcript = fh.read()
-
-        with gentle.resampled(audiofile) as wavfile:
+        
+        with gentle.resampled(audiofile, lang) as wavfile:
             logging.info("starting alignment for %s" % (audiofile))
-            aligner = gentle.ForcedAligner(resources, transcript, nthreads=args.nthreads, disfluency=args.disfluency, conservative=args.conservative, disfluencies=disfluencies)
+            print args
+            aligner = gentle.ForcedAligner(resources, transcript, nthreads=args.nthreads, disfluency=args.disfluency, conservative=args.conservative, disfluencies=disfluencies, lang=lang)
             result = aligner.transcribe(wavfile, progress_cb=on_progress, logging=logging)
-
+        
         with open(txtfile+'.align.json', 'w') as fh:
             fh.write(result.to_json(indent=2))
         if args.output:
             logging.info("output written to %s" % (args.output))
-    except Exception as ex:
+    except IOError as ex:
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         logging.info("{} failed".format(txtfile))
         logging.info(message)
-        continue
 
+for item in scp:
+    p = multiprocessing.Process(target=align_pair, args=(item,))
+    p.daemon=True
+    p.start()
+    p.join(timeout=500)
+    try:
+        os.kill(p.pid, signal.SIGKILL)
+    except OSError:
+        pass

43de3ca078a1da5f84512ceb1f65f2a9a6f05883
Akshat Dewan <akshatdewan@gmail.com>
2020-04-03 18:32:13 +0200

    Dockerization for gentle:en_fr

--- ext/install_kaldi-5.4.sh
+++ ext/install_kaldi-5.4.sh
@@ -3,9 +3,11 @@
 # Prepare Kaldi
 cd kaldi-5.4/tools
 #make clean
-make blas all -j 48
+make -j 48 openfst OPENFST_VERSION=1.6.7
+make -j 48 openblas 
 cd ../src
 make clean
- ./configure --shared --cudatk-dir=/usr/local/cuda-8.0
+ ./configure --static --static-math=yes --static-fst=yes --use-cuda=no
 make depend -j 48
 cd ../../
+ #./configure --shared --cudatk-dir=/usr/local/cuda-8.0

323c5b6c49c25ac12ad4a1c7d886d1373c67771f
Akshat Dewan <akshatdewan@gmail.com>
2020-05-19 11:32:04 +0200

    number of jobs argument exposed to Dockerfile

--- ext/install_kaldi-5.4.sh
+++ ext/install_kaldi-5.4.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+jobs=$1
 # Prepare Kaldi
 cd kaldi-5.4/tools
 #make clean
-make -j 48 openfst OPENFST_VERSION=1.6.7
-make -j 48 openblas 
+make -j $jobs openfst OPENFST_VERSION=1.6.7
+make -j $jobs openblas 
 cd ../src
 make clean
  ./configure --static --static-math=yes --static-fst=yes --use-cuda=no
-make depend -j 48
+make depend -j $jobs
 cd ../../
  #./configure --shared --cudatk-dir=/usr/local/cuda-8.0
--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Prepare Kaldi
+jobs=$1
 cd kaldi/tools
 make clean
-make atlas openfst OPENFST_VERSION=1.4.1 -j 48
+make atlas openfst OPENFST_VERSION=1.4.1 -j $jobs
 cd ../src
 make clean
 ./configure --static --static-math=yes --static-fst=yes --use-cuda=no
-make depend -j 48
+make depend -j $jobs
 cd ../../
